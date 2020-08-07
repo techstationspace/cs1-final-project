f98e7727e43bec5e0ef4e6e7aa11a1d2e29b44e5
Alessandro Mazzucchelli <mazzucchelli.work@gmail.com>
2020-08-07 12:38:37 +0000

    add viewTopic component, connected backend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './App.css';
 
+import ViewTopic from './pages/curriculum/createTopic';
+
 function App() {
   return (
     <div className='App'>
       <h1>Frontend CS Final Project</h1>
+      <ViewTopic />
     </div>
   );
 }
