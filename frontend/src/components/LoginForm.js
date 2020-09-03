373c297521efd349b9bda0c9a0b44ccc3bae9758
Andrea <littlehooah@gmail.com>
2020-09-03 22:25:16 +0200

    add logic to LogicForm and switches from class to function component

--- frontend/src/components/LoginForm.js
+++ frontend/src/components/LoginForm.js
@@ -1,28 +1,54 @@
-import React from 'react';
-import { Container, Paper, withStyles, Grid, TextField, Button, FormControlLabel, Checkbox } from '@material-ui/core';
-import { Face, Fingerprint } from '@material-ui/icons'
-const styles = theme => ({
+import React, { useState } from 'react';
+import { Container, Paper, withStyles, Grid, TextField, Button, FormControlLabel, Checkbox, makeStyles } from '@material-ui/core';
+import { Face, Fingerprint, PinDropSharp } from '@material-ui/icons'
+
+const styled = makeStyles({
     margin: {
-        margin: theme.spacing.unit * 2,
+        margin: "1rem",
+    },
+    marginTop: {
+        marginTop: "1rem"
     },
     padding: {
-        padding: theme.spacing.unit
+        padding: "1rem"
     }
 });
 
-class LoginForm extends React.Component {
-    render() {
-        const { classes } = this.props;
-        return (
-          <Container maxWidth="sm">
-            <Paper className={classes.padding} elevation={3}>
+const LoginForm = ({loginDataEx, onSubmit}) => {
+
+    const classes = styled()
+
+    const [loginData, setLoginData] = useState({ username:loginDataEx.username, password: loginDataEx.password })
+    const updateField = (element) => {
+        setLoginData(
+            { ...loginData, [element.target.id]: element.target.value }
+        )
+    }
+
+    //Remember me function needs to be implemented <------------------
+
+    //Forgot Password function needs to be implemented <--------------
+
+ 
+    return (
+        <Container maxWidth="sm">
+            <Paper className={classes.padding}elevation={3}>
                 <div className={classes.margin}>
                     <Grid container spacing={8} alignItems="flex-end">
                         <Grid item>
                             <Face />
                         </Grid>
                         <Grid item md={true} sm={true} xs={true}>
-                            <TextField id="username" label="Username" type="email" fullWidth autoFocus required />
+                            <TextField
+                                id="username"
+                                label="Username"
+                                type="email"
+                                value={loginData.username}
+                                onInput={updateField}
+                                fullWidth
+                                autoFocus
+                                required
+                            />
                         </Grid>
                     </Grid>
                     <Grid container spacing={8} alignItems="flex-end">
@@ -30,29 +56,47 @@ class LoginForm extends React.Component {
                             <Fingerprint />
                         </Grid>
                         <Grid item md={true} sm={true} xs={true}>
-                            <TextField id="username" label="Password" type="password" fullWidth required />
+                            <TextField 
+                            id="password"
+                            label="Password"
+                            type="password"
+                            value={loginData.password}
+                            onInput={updateField}
+                            fullWidth
+                            required />
                         </Grid>
                     </Grid>
                     <Grid container alignItems="center" justify="space-between">
-                        <Grid item>
+                        <Grid item className={classes.marginTop}>
                             <FormControlLabel control={
                                 <Checkbox
                                     color="primary"
                                 />
                             } label="Remember me" />
                         </Grid>
-                        <Grid item>
-                            <Button disableFocusRipple disableRipple style={{ textTransform: "none" }} variant="text" color="primary">Forgot password ?</Button>
+                        <Grid item className={classes.marginTop}>
+                            <Button
+                            disableFocusRipple 
+                            disableRipple 
+                            style={{ textTransform: "none" }} 
+                            variant="text" color="primary">
+                            Forgot password ?
+                            </Button>
                         </Grid>
                     </Grid>
                     <Grid container justify="center" style={{ marginTop: '10px' }}>
-                        <Button variant="outlined" color="primary" style={{ textTransform: "none" }}>Login</Button>
+                        <Button 
+                        variant="outlined" 
+                        color="primary" 
+                        style={{ textTransform: "none" }} 
+                        onClick={() => onSubmit(loginData)}> 
+                        Login 
+                        </Button>
                     </Grid>
                 </div>
             </Paper>
-          </Container>  
-        );
-    }
+        </Container>
+    );
 }
 
-export default withStyles(styles)(LoginForm);
+export default LoginForm;
