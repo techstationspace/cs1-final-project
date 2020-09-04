0a6ed7da52893a160c6b23e0a9a7f4f89d13f761
Mirco Cotugno <mirco@pitchtarget.com>
2020-09-04 11:41:39 +0200

    Wip manage courses

--- frontend/src/routes/Course.js
+++ frontend/src/routes/Course.js
@@ -1,10 +1,11 @@
 import React from "react";
 
 export default function Course(data) {
+  console.log(data);
   return (
     <>
       <h1>{data.title}</h1>
-      <p>{data.content}</p>
+      <p>{data.description}</p>
     </>
   );
 }
