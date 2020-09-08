cde4d76fa65c683572cc5c48fde9501d26dbf5b0
yevgeniyakulikova <eugikul44@gmail.com>
2020-09-08 12:54:59 +0200

    compare bcrypt password with db

--- backend/routes/api/login.js
+++ backend/routes/api/login.js
@@ -1,5 +1,6 @@
 const { User } = require('../../models/models.js');
 const {validateUsername} = require('../../utils/auth.js');
+const bcrypt = require("bcryptjs")
 
 module.exports = (app) => {
   app.post('/api/register', async (req, res ) => {
@@ -10,13 +11,28 @@ module.exports = (app) => {
      res.status(400).json({success: false, message: "Il nome è stato preso"})
      return
     }
-    const userDB = new User({username : user.username, password : user.password, role : user.role})
+    const hashedPassword = await bcrypt.hash(user.password, 12)
+    const userDB = new User({username : user.username, password : hashedPassword, role : user.role})
     await userDB.save()
     res.status(200).json({success: true, message: "L'utente è registrato"})
   });
-  app.post('/api/login', (req, res)=>{
+  app.post('/api/login', async (req, res) => {
     //Here we login the users
-
+    const user = req.body.user
+    const isUsernameTaken = await validateUsername(user.username)
+    if (!isUsernameTaken) {
+     res.status(400).json({success: false, message: "Il nome dell'utente non esiste"})
+     return 
+    }
+    const clientInfo = await User.findOne({username: user.username})    
+    const isPasswordCorrect = await bcrypt.compare(user.password, clientInfo.password);
+   if(!isPasswordCorrect) {
+     res.status(400).json({success: false, message: "la password non è coretta, stupidino"})
+     return
+   }
+    console.log(clientInfo + "login eseguito")
+    res.status(200).json({success: true, messagge: "bravo andrux!"})
+   //qui generiamo e restituiamo il token JWT 
   })
   // Insert other routes here
 
