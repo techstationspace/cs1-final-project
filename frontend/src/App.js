8ae8a87d575833bee45b8dbddb6b555370f11231
Mirco Cotugno <mirco@pitchtarget.com>
2020-09-17 16:02:03 +0200

    start to connect

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,5 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
+import axios from "axios";
 import "./App.css";
 import AppView from "./components/AppView";
 import LoginView from "./components/LoginView";
@@ -7,17 +8,20 @@ function App() {
   const [isLogged, setIsLogged] = useState(false);
 
   const checkLogin = (loginData) => {
-    const user = loginData.username;
-    const pwd = loginData.password;
-
-    // chiamata Autenticazione
-
-    if(!!user && !!pwd) {
-      setIsLogged(true);
-    } else {
-      setIsLogged(false);
-    }
-  }
+    // const config = {
+    //   headers: { Authorization: `Bearer ${token}` },
+    // };
+    const bodyParameters = {
+      user: loginData,
+    };
+    axios.post(
+      "http://localhost:4000/api/login",
+      bodyParameters
+      // config
+    )
+      .then(console.log)
+      .catch(console.log);
+  };
 
   return (
     <>
@@ -25,10 +29,7 @@ function App() {
         href="https://fonts.googleapis.com/icon?family=Material+Icons"
         rel="stylesheet"
       ></link>
-      { isLogged
-        ? <AppView />
-        : <LoginView submitForm={(e) => checkLogin(e)} />
-      }
+      {false ? <AppView /> : <LoginView submitForm={(e) => checkLogin(e)} />}
     </>
   );
 }
