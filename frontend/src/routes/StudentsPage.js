11b7d4328a6831cd83162ba4c23538398f952931
Mirco Cotugno <mirco@pitchtarget.com>
2020-09-17 10:04:24 +0200

    fix StudentPage after rebase

--- frontend/src/routes/StudentsPage.js
+++ frontend/src/routes/StudentsPage.js
@@ -1,8 +1,7 @@
-<<<<<<< HEAD
-import React, {useState} from "react";
+import React, { useState } from "react";
 import nextId from "react-id-generator";
 import FormSendCandidacy from "../components/FormSendCandidacy";
-import { Grid, Button} from "@material-ui/core";
+import { Grid, Button } from "@material-ui/core";
 import { Switch, Route, Link } from "react-router-dom";
 //import Course from "./Course";
 import Student from "./Profile";
@@ -18,80 +17,79 @@ let mokData = {
   address: "Via Roma 123",
   municipality: "Vicenza",
   zipCode: "36100",
-  nationality: { code: 'AS', label: 'American Samoa', phone: '1-684' },
+  nationality: { code: "AS", label: "American Samoa", phone: "1-684" },
   termsCondition: "true",
   privacy: "false",
   images: "true",
-  birthday: "1999-05-12"
+  birthday: "1999-05-12",
 };
-let students= [];
-=======
-import React, { useState } from "react";
-import { Grid, Button } from "@material-ui/core";
->>>>>>> delete ButtonNav
+let students = [];
 
 export default function StudentsPage() {
-  let [data , setForm]= React.useState(mokData);
+  let [data, setForm] = React.useState(mokData);
   const [openLateralView, setOpenLateralView] = useState(false);
   const [studentId, setStudentId] = useState("");
 
- const submitForm = (data) => {
+  const submitForm = (data) => {
     /*let newFormData = {
       id: nextId("course-"),
       path: title.replace(/\s+/g, '-').toLowerCase(),
       title: title,
       description: description,
     };*/
-    let newFormData = data
-    newFormData.id = nextId("course-")
-    
-    if(studentId) {
+    let newFormData = data;
+    newFormData.id = nextId("course-");
+
+    if (studentId) {
       let student = findStudent(studentId);
-      student = copyDataStudent(newFormData)
-      console.log(student)
+      student = copyDataStudent(newFormData);
+      console.log(student);
     } else {
       students.push(newFormData);
     }
     //console.log(courses);
     setForm("");
-    setStudentId("")
+    setStudentId("");
   };
-{/*  const abortForm = () => {
+  {
+    /*
+    const abortForm = () => {
     setTitle("");
     setDescription("");
     setCourseId("");
     setOpenLateralView(false);
   }
-*/}
-  const copyDataStudent= (data) =>{
-    let value={}
-    for(const property in data){
-      value[property]= data[property];
+  */
+  }
+  const copyDataStudent = (data) => {
+    let value = {};
+    for (const property in data) {
+      value[property] = data[property];
     }
-    return value
-  }
+    return value;
+  };
   const findStudent = (name) => {
-    let [studentToEdit] = students.filter(student => student.name === name);
+    let [studentToEdit] = students.filter((student) => student.name === name);
     return studentToEdit;
-  }
+  };
 
   const editCourse = (name) => {
     setStudentId(name);
     let student = findStudent(name);
-    setForm(student)
+    setForm(student);
     setOpenLateralView(true);
-  }
+  };
   return (
     <>
-      <h1>Student Page</h1>
-      <Button onClick={() => setOpenLateralView(true)}>
-        New Student
-      </Button>
+      <h1>Students Page</h1>
+      <Button onClick={() => setOpenLateralView(true)}>New Student</Button>
       <Grid container spacing={3}>
         {students.map((student, idx) => (
           <Grid key={idx} item md={6} lg={4}>
             <Link to={`student/${student.name}`}>
-              <h2>{student.name} {student.surname}</h2>
+              <h2>
+                {student.name} {student.surname}
+              </h2>
             </Link>
             <Button
               variant="contained"
@@ -107,9 +105,14 @@ export default function StudentsPage() {
         onClose={() => setOpenLateralView(false)}
       >
         {/* Here start the children inside LateralView */}
-        
-        <FormSendCandidacy data={data} onSubmit={(e) => {submitForm(e)
-           setOpenLateralView(false)}}/>
+
+        <FormSendCandidacy
+          data={data}
+          onSubmit={(e) => {
+            submitForm(e);
+            setOpenLateralView(false);
+          }}
+        />
       </LateralView>
       <Switch>
         {students.map((student, i) => (
@@ -117,5 +120,14 @@ export default function StudentsPage() {
         ))}
       </Switch>
     </>
+  );
+}
+
+function StudentPage(student) {
+  return (
+    <Route
+      path={`students/${student.name}`}
+      render={(props) => <Student {...props} data={student} />}
+    />
   );
 }
