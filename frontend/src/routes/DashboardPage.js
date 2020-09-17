1a95c116323503fd751f925bb5ab29946fe872ab
Mirco Cotugno <mirco@pitchtarget.com>
2020-09-17 10:36:41 +0200

    prettify Dashboard code

--- frontend/src/routes/DashboardPage.js
+++ frontend/src/routes/DashboardPage.js
@@ -1,25 +1,23 @@
 import React from "react";
 import { Grid } from "@material-ui/core";
-import { ListItemAvatar, Avatar} from "@material-ui/core";
+import { ListItemAvatar, Avatar } from "@material-ui/core";
 
-
-export default function CoachesPage() {
-
+export default function DashboardPage() {
   return (
     <>
-      <Grid container direction="row" alignItems="center" justify="space-between" >
-        <Grid item >
-        name 
-  </Grid>
-  <Grid item   >
-          surname
-          </Grid>
-          <Grid style={{marginRight:"0.7rem"}} item >
+      <Grid
+        container
+        direction="row"
+        alignItems="center"
+        justify="space-between"
+      >
+        <Grid item>name</Grid>
+        <Grid item>surname</Grid>
+        <Grid style={{ marginRight: "0.7rem" }} item>
           <ListItemAvatar>
-        <Avatar alt="img" src={process.env.PUBLIC_URL + ""} />
-      </ListItemAvatar>
-
-          </Grid>
+            <Avatar alt="img" src={process.env.PUBLIC_URL + ""} />
+          </ListItemAvatar>
+        </Grid>
       </Grid>
     </>
   );
