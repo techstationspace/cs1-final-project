f3ccd4b309fe1999f851b8b84b732febb478943b
MatteoScheven <matteo.schieven@gmail.com>
2020-09-23 15:48:21 +0200

    add patch user in registrationPt2 for fix connection db

--- backend/routes/api/login.js
+++ backend/routes/api/login.js
@@ -5,20 +5,6 @@ const bcrypt = require("bcryptjs")
 const jwt = require("jsonwebtoken")
 
 module.exports = (app) => {
-  app.post('/api/register', async (req, res) => {
-    //Here we register the users
-    const user = req.body.user
-    //const isUsernameTaken = await validateUsername(user.username)
-    const isEmailTaken = await validateEmail(user.email, user.verifiedEmail)
-    if (isEmailTaken().isTaken) {
-      res.status(400).json({ success: false, message: "Il email è stato preso" })
-      return
-    }
-    const hashedPassword = await bcrypt.hash(user.password, 12)
-    const userDB = new User({ email: user.email, validateEmail: user.validateEmail, password: hashedPassword, role: user.role })
-    await userDB.save({ validateBeforeSave: false }) //Attenzione salta la validazione al DB perchè "name" "surname" su User schema sono required
-    res.status(200).json({ success: true, message: "L'utente è registrato" })
-  });
   
   app.post('/api/login', async (req, res) => {
     //Here we login the users
--- backend/routes/api/registrationPt2.js
+++ backend/routes/api/registrationPt2.js
@@ -1,10 +1,12 @@
 const { User } = require('../../models/models.js');
-const sendEmail = require("../../utils/sendemail")
+const sendEmail = require("../../utils/sendemail");
+const bcrypt = require("bcryptjs");
+
 
 module.exports = (app) => {
   app.get('/api/users/register/:id', async (req, res ) => {
     //Possibile uso di params per identificare l'utente da link ?
-    const userQuery = await User.findOne({id: req.params.id})
+    const userQuery = await User.findOne({_id: req.params.id})
     if (!userQuery) {
       res.status(400).json({success: false, message: "Selected e-mail not found"})
       return
@@ -14,32 +16,42 @@ module.exports = (app) => {
 
 
   app.patch('/api/users/register/:id', async (req, res ) => {
-    const user = req.body.user
+    console.log(req.body)
+    console.log(req.params)
+    //const user = req.body.payload
+    const user =req.body.user
+    const hashedPassword = await bcrypt.hash(user.password, 12)
     User.findOneAndUpdate(
-      {id: req.params.id}, 
-      {
+      {_id: req.params.id}, 
+      { name: user.name,
+        surname: user.surname,
+        password: hashedPassword,
+        gender: user.gender,
+        address: user.address,
+        municipality: user.municipality,
+        zipCode: user.zipCode,
+        nationality: user.nationality,
+        termsCondition: user.termsCondition,
+        privacy: user.privacy,
+        images: user.images,
+        birthday: user.birthday,
+        verifiedEmail: true,
+      })
+      .then((user) =>{
+        if(user == null){
+          res.status(400).json({ success: false, message: "User doesn't make first registration" });
+        }
+        else{
+          res.status(200).json({ success: true, message: "User updated" })
+        }
+      })
+      .catch((err) => res.status(400).json({ success: false, message: err.message }));
 
-      }
-    )
 
     //Db save
-    /*const userDB = new User(user
-      {password: user.password,
-      gender: user.gender,
-      address: user.address,
-      municipality: user.municipality,
-      zipCode: user.zipCode,
-      nationality: user.nationality,
-      termsCondition: user.termsCondition,
-      privacy: user.privacy,
-      images: user.images,
-      birthday: user.birthday}      
-    )
-    NB: gestione dati? se un campo non è presente va tutto in vacca...ha senso scorporare l'oggetto user?
-    Perchè con "new User sopra si crea un campo _id che è incompatibile con _id già presente nel documento da aggiornare" mando allora direttamente {user}*/
-    await User.updateOne({email: user.email}, user)
+    //await User.updateOne({email: user.email}, user)
     
-    res.status(200).json({success: true, message: "L'utente ha completato la registrazione del secondo step"})
+    //res.status(200).json({success: true, message: "L'utente ha completato la registrazione del secondo step"})
 
     //E-mail message settings
     const message = {
