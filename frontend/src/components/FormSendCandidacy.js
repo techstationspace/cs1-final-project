4351219c1e4774acc15fb2ed83c779f98b086a38
Mirco Cotugno <mirco@pitchtarget.com>
2020-09-29 13:11:13 +0200

    beautify condidacy form

--- frontend/src/components/FormSendCandidacy.js
+++ frontend/src/components/FormSendCandidacy.js
@@ -33,23 +33,25 @@ function FormSendCandidacy({ data, onSubmit }) {
   const classes = useStyles();
   const [correctPassword, setCorrectPassword] = useState(false);
   const [nationality, setValue] = useState(entryNationality);
-  const [birthday, setBirthday] = useState(moment(data.birthday).format("YYYY-MM-DD"))
+  const [birthday, setBirthday] = useState(
+    moment(data.birthday).format("YYYY-MM-DD")
+  );
   console.log(birthday);
   const [form, setForm] = useState({
-    name: !!data.name? data.name : "",
-    surname: !!data.surname? data.surname : "",
-    email: !!data.email? data.email : "",
-    password: !!data.password? data.password : "",
-    gender: !!data.gender? data.gender : "",
-    address: !!data.address? data.address : "",
-    municipality: !!data.municipality? data.municipality : "",
-    zipCode: !!data.zipCode? data.zipCode : "",
-    nationality: !!data.nationality? data.nationality : "",
-    termsCondition: !!data.termsCondition? data.termsCondition : "",
-    privacy: !!data.privacy? data.privacy : "",
-    images: !!data.images? data.images : "",
-    birthday: !!data.birthday? data.birthday : "",
-    verifiedEmail: !!data.verifiedEmail? data.verifiedEmail : "",
+    name: !!data.name ? data.name : "",
+    surname: !!data.surname ? data.surname : "",
+    email: !!data.email ? data.email : "",
+    password: !!data.password ? data.password : "",
+    gender: !!data.gender ? data.gender : "",
+    address: !!data.address ? data.address : "",
+    municipality: !!data.municipality ? data.municipality : "",
+    zipCode: !!data.zipCode ? data.zipCode : "",
+    nationality: !!data.nationality ? data.nationality : "",
+    termsCondition: !!data.termsCondition ? data.termsCondition : "",
+    privacy: !!data.privacy ? data.privacy : "",
+    images: !!data.images ? data.images : "",
+    birthday: !!data.birthday ? data.birthday : "",
+    verifiedEmail: !!data.verifiedEmail ? data.verifiedEmail : "",
   });
   const [password, setPassword] = useState("");
   const [confPassword, setConfPassword] = useState("");
@@ -58,7 +60,7 @@ function FormSendCandidacy({ data, onSubmit }) {
     form.nationality = nationality;
     form.password = password;
     event.preventDefault();
-    form.birthday= birthday /* moment(birthday).format('yyyy-MM-dd\'T\'HH:mm:ss.SSS\'Z\''); */
+    form.birthday = birthday; /* moment(birthday).format('yyyy-MM-dd\'T\'HH:mm:ss.SSS\'Z\''); */
     console.log(birthday);
     console.log(form.birthday);
     console.log(form);
@@ -73,185 +75,177 @@ function FormSendCandidacy({ data, onSubmit }) {
     });
   }
   return (
-    <div className={classes.padding}>
-      <form onSubmit={handleSubmit}>
-        <Grid container direction="row" spacing={4}>
-          <Grid xs={3} item>
-            <TextField
-              fullWidth
-              required
-              name="name"
-              value={form.name}
-              onInput={updateData}
-              label="Name"
-            />
-          </Grid>
-          <Grid xs={3} item>
-            <TextField
-              fullWidth
-              required
-              name="surname"
-              value={form.surname}
-              onInput={updateData}
-              label="Surname"
-            />
-          </Grid>
-          <Grid item xs={6}></Grid>
-          <Grid xs={6} item>
-            <TextField
-              disabled={!!data.email? true : false}
-              fullWidth
-              required
-              type="email"
-              name="email"
-              value={form.email}
-              onInput={updateData}
-              //onInput={(e) => setForm(e.target.value)}
-              label="E-mail"
-            />
-          </Grid>
-        </Grid>
-        <Grid
-          container
-          className={classes.marginTop}
-          direction="row"
-          spacing={3}
-        >
-          <Grid item>
+    <form onSubmit={handleSubmit}>
+      <Grid container direction="row" spacing={4}>
+        <Grid xs={12} sm={6} item>
           <TextField
-              value={data.birthday}
-              onInput={(e) => setBirthday(e.target.value)}
-              required
-              name="birthday"
-              label="Birthday"
-              type="date"
-              InputLabelProps={{
-                shrink: true,
-              }}
-            />
-          </Grid>
-        </Grid>
-        <Grid container className={classes.marginTop}>
-          <Grid item>
-            <FormControl component="fieldset" required name="gender">
-              <FormLabel component="legend">Gender</FormLabel>
-              <RadioGroup
-                row
-                aria-label="gender"
-                value={form.gender}
-                onChange={updateData}
-                name="gender"
-              >
-                <FormControlLabel
-                  value="female"
-                  control={<Radio />}
-                  label="Female"
-                />
-                <FormControlLabel
-                  value="male"
-                  control={<Radio />}
-                  label="Male"
-                />
-                <FormControlLabel
-                  value="other"
-                  control={<Radio />}
-                  label="Other"
-                />
-                <FormControlLabel
-                  value="notSpecified"
-                  control={<Radio />}
-                  label="Not specified"
-                />
-              </RadioGroup>
-            </FormControl>
-          </Grid>
-        </Grid>
-        <Grid container direction="row" spacing={4}>
-          <Grid xs={3} item>
-            <TextField
-              fullWidth
-              required
-              name="municipality"
-              value={form.municipality}
-              onInput={updateData}
-              label="Municipality"
-            />
-          </Grid>
-          <Grid xs={3} item>
-            <TextField
-              fullWidth
-              required
-              name="zipCode"
-              type="number"
-              value={form.zipCode}
-              onInput={updateData}
-              label="Zip Code"
-            />
-          </Grid>
-          <Grid item xs={6}></Grid>
-          <Grid xs={6} item>
-            <TextField
-              multiline
-              name="address"
-              fullWidth
-              value={form.address}
-              onInput={updateData}
-              label="Address"
-              required
-            />
-          </Grid>
-        </Grid>
-        <Grid container className={classes.marginTop}>
-          <Autocomplete
-            name="nationality"
-            value={nationality}
-            onChange={(event, newValue) => {
-              setValue(newValue);
-            }}
-            style={{ width: 300 }}
-            options={countries}
-            getOptionSelected={(option) => option.label}
-            classes={{
-              option: classes.option,
-            }}
-            autoHighlight
-            getOptionLabel={(option) => option.label}
-            renderOption={(option) => (
-              <React.Fragment>{option.label}</React.Fragment>
-            )}
-            renderInput={(params) => (
-              <TextField
-                {...params}
-                label="Nationality"
-                inputProps={{
-                  ...params.inputProps,
-                  autoComplete: "new-password", // disable autocomplete and autofill
-                }}
-              />
-            )}
+            fullWidth
+            required
+            name="name"
+            value={form.name}
+            onInput={updateData}
+            label="Name"
           />
         </Grid>
-        <Grid container className={classes.marginTop} spacing={3}>
-          <Grid item>
+        <Grid xs={12} sm={6} item>
+          <TextField
+            fullWidth
+            required
+            name="surname"
+            value={form.surname}
+            onInput={updateData}
+            label="Surname"
+          />
+        </Grid>
+        <Grid xs={12} item>
+          <TextField
+            disabled={!!data.email ? true : false}
+            fullWidth
+            required
+            type="email"
+            name="email"
+            value={form.email}
+            onInput={updateData}
+            //onInput={(e) => setForm(e.target.value)}
+            label="E-mail"
+          />
+        </Grid>
+      </Grid>
+      <Grid container className={classes.marginTop} direction="row" spacing={3}>
+        <Grid item xs={6}>
+          <TextField
+            value={data.birthday}
+            onInput={(e) => setBirthday(e.target.value)}
+            required
+            fullWidth
+            name="birthday"
+            label="Birthday"
+            type="date"
+            InputLabelProps={{
+              shrink: true,
+            }}
+          />
+        </Grid>
+      </Grid>
+      <Grid container className={classes.marginTop}>
+        <Grid item xs={12}>
+          <FormControl component="fieldset" required name="gender" fullWidth>
+            <FormLabel component="legend">Gender</FormLabel>
+            <RadioGroup
+              row
+              aria-label="gender"
+              value={form.gender}
+              onChange={updateData}
+              name="gender"
+            >
+              <FormControlLabel
+                value="female"
+                control={<Radio />}
+                label="Female"
+              />
+              <FormControlLabel value="male" control={<Radio />} label="Male" />
+              <FormControlLabel
+                value="other"
+                control={<Radio />}
+                label="Other"
+              />
+              <FormControlLabel
+                value="notSpecified"
+                control={<Radio />}
+                label="Not specified"
+              />
+            </RadioGroup>
+          </FormControl>
+        </Grid>
+      </Grid>
+      <Grid container direction="row" spacing={4}>
+        <Grid xs={12} item>
+          <TextField
+            fullWidth
+            required
+            name="municipality"
+            value={form.municipality}
+            onInput={updateData}
+            label="Municipality"
+          />
+        </Grid>
+        <Grid xs={6} item>
+          <TextField
+            fullWidth
+            required
+            name="zipCode"
+            type="number"
+            value={form.zipCode}
+            onInput={updateData}
+            label="Zip Code"
+          />
+        </Grid>
+        <Grid xs={12} item>
+          <TextField
+            multiline
+            name="address"
+            fullWidth
+            value={form.address}
+            onInput={updateData}
+            label="Address"
+            required
+          />
+        </Grid>
+      </Grid>
+      <Grid container className={classes.marginTop}>
+        <Autocomplete
+          name="nationality"
+          value={nationality}
+          onChange={(event, newValue) => {
+            setValue(newValue);
+          }}
+          style={{ width: 300 }}
+          options={countries}
+          getOptionSelected={(option) => option.label}
+          classes={{
+            option: classes.option,
+          }}
+          autoHighlight
+          getOptionLabel={(option) => option.label}
+          renderOption={(option) => (
+            <React.Fragment>{option.label}</React.Fragment>
+          )}
+          renderInput={(params) => (
             <TextField
-              name="password"
-              label="Password"
-              type="password"
-              required
-              onInput={(e) => setPassword(e.target.value)}
-              onChange={(e) =>
-                confPassword !== e.target.value
-                  ? setCorrectPassword(false)
-                  : setCorrectPassword(true)
-              }
+              {...params}
+              label="Nationality"
+              inputProps={{
+                ...params.inputProps,
+                autoComplete: "new-password", // disable autocomplete and autofill
+              }}
             />
-          </Grid>
-          <Grid item>
+          )}
+        />
+      </Grid>
+      <Grid container className={classes.marginTop} spacing={3}>
+        <Grid item xs={6}>
+          <TextField
+            name="password"
+            label="Password"
+            type="password"
+            fullWidth
+            required
+            onInput={(e) => setPassword(e.target.value)}
+            onChange={(e) =>
+              confPassword !== e.target.value
+                ? setCorrectPassword(false)
+                : setCorrectPassword(true)
+            }
+          />
+        </Grid>
+        {!!password && (
+          <Grid item xs={6}>
             <TextField
               error={!correctPassword}
               id="confirm-password"
               label="Confirm Password"
               type="password"
+              fullWidth
               required
               onInput={(e) => setConfPassword(e.target.value)}
               onChange={(e) =>
@@ -261,101 +255,90 @@ function FormSendCandidacy({ data, onSubmit }) {
               }
             />
           </Grid>
-        </Grid>
-        <Grid container className={classes.marginTop}>
-          <FormControl component="fieldset">
-            <FormLabel component="legend">
-              Accept the terms and condition
-            </FormLabel>
-            <RadioGroup
-              row
-              aria-label="gender"
-              name="termsCondition"
-              //defaultValue={form.termsCondition}
-              value={form.termsCondition}
-              onChange={updateData}
-              required
-            >
-              <FormControlLabel
-                value="true"
-                control={<Radio />}
-                label="Accept"
-              />
-              <FormControlLabel
-                value="false"
-                control={<Radio />}
-                label="No accept"
-              />
-            </RadioGroup>
-          </FormControl>
-        </Grid>
-        <Grid container className={classes.marginTop}>
-          <FormControl component="fieldset" required>
-            <FormLabel component="legend">Accept the privacy policy</FormLabel>
-            <RadioGroup
-              row
-              aria-label="gender"
-              name="privacy"
-              //defaultValue={form.privacy}
-              value={form.privacy}
-              onChange={updateData}
-            >
-              <FormControlLabel
-                value="true"
-                control={<Radio />}
-                label="Accept"
-              />
-              <FormControlLabel
-                value="false"
-                control={<Radio />}
-                label="No accept"
-              />
-            </RadioGroup>
-          </FormControl>
-        </Grid>
-        <Grid container className={classes.marginTop}>
-          <FormControl component="fieldset" required>
-            <FormLabel component="legend">
-              Agreement to the use of images
-            </FormLabel>
-            <RadioGroup
-              row
-              aria-label="gender"
-              name="images"
-              //defaultValue={form.images}
-              value={form.images}
-              onChange={updateData}
-            >
-              <FormControlLabel
-                value="true"
-                control={<Radio />}
-                label="Accept"
-              />
-              <FormControlLabel
-                value="false"
-                control={<Radio />}
-                label="No accept"
-              />
-            </RadioGroup>
-          </FormControl>
-        </Grid>
-        <Button
-          className={classes.marginTop}
-          disabled={
-            correctPassword === true &&
-            form.privacy === "true" &&
-            form.termsCondition === "true"
-              ? false
-              : true
-          }
-          type="submit"
-          variant="outlined"
-          style={{margin:"1em", color: "white", backgroundColor: "#874399" }}
-        >
-          Send Form
-        </Button>
-      </form>
-    </div>
+        )}
+      </Grid>
+      <Grid container className={classes.marginTop}>
+        <FormControl component="fieldset">
+          <FormLabel component="legend">
+            Accept the terms and condition
+          </FormLabel>
+          <RadioGroup
+            row
+            aria-label="gender"
+            name="termsCondition"
+            //defaultValue={form.termsCondition}
+            value={form.termsCondition}
+            onChange={updateData}
+            required
+          >
+            <FormControlLabel value="true" control={<Radio />} label="Accept" />
+            <FormControlLabel
+              value="false"
+              control={<Radio />}
+              label="No accept"
+            />
+          </RadioGroup>
+        </FormControl>
+      </Grid>
+      <Grid container className={classes.marginTop}>
+        <FormControl component="fieldset" required>
+          <FormLabel component="legend">Accept the privacy policy</FormLabel>
+          <RadioGroup
+            row
+            aria-label="gender"
+            name="privacy"
+            //defaultValue={form.privacy}
+            value={form.privacy}
+            onChange={updateData}
+          >
+            <FormControlLabel value="true" control={<Radio />} label="Accept" />
+            <FormControlLabel
+              value="false"
+              control={<Radio />}
+              label="No accept"
+            />
+          </RadioGroup>
+        </FormControl>
+      </Grid>
+      <Grid container className={classes.marginTop}>
+        <FormControl component="fieldset" required>
+          <FormLabel component="legend">
+            Agreement to the use of images
+          </FormLabel>
+          <RadioGroup
+            row
+            aria-label="gender"
+            name="images"
+            //defaultValue={form.images}
+            value={form.images}
+            onChange={updateData}
+          >
+            <FormControlLabel value="true" control={<Radio />} label="Accept" />
+            <FormControlLabel
+              value="false"
+              control={<Radio />}
+              label="No accept"
+            />
+          </RadioGroup>
+        </FormControl>
+      </Grid>
+      <Button
+        className={classes.marginTop}
+        disabled={
+          correctPassword === true &&
+          form.privacy === "true" &&
+          form.termsCondition === "true"
+            ? false
+            : true
+        }
+        fullWidth
+        type="submit"
+        variant="outlined"
+        style={{ color: "white", backgroundColor: "#874399" }}
+      >
+        Send Form
+      </Button>
+    </form>
   );
 }
 export default FormSendCandidacy;
--- frontend/src/routes/Candidacy-Page.js
+++ frontend/src/routes/Candidacy-Page.js
@@ -1,7 +1,7 @@
-import React, { useState, useEffect} from "react";
-import { Paper, Container, makeStyles } from "@material-ui/core";
+import React, { useState, useEffect } from "react";
+import { Paper, Container, Grid, makeStyles } from "@material-ui/core";
 import FormSendCandidacy from "../components/FormSendCandidacy";
-import ConfirmSingIn from "../components/ConfirmSingIn"
+import ConfirmSingIn from "../components/ConfirmSingIn";
 import axios from "axios";
 import { useParams } from "react-router-dom";
 
@@ -13,7 +13,7 @@ const useStyles = makeStyles((theme) => ({
     padding: "3rem",
   },
   paddingForm: {
-    padding: "1rem",
+    padding: "2rem",
   },
 }));
 let nameCourse = "CODING SCHOOL";
@@ -22,53 +22,60 @@ function CandidacyPage() {
   const classes = useStyles();
   const [userInfo, setUserInfo] = useState();
   const [sendData, setSendData] = useState(false);
-  const {userId}= useParams()
+  const { userId } = useParams();
   const fetchUser = async () => {
     await fetch(`http://localhost:4000/api/users/register/${userId}`)
-    .then((response) => response.json())
-     .then(function (response) {
-      console.log("ok request");
-      console.log(response.payload)
-      setUserInfo(response.payload);
-    })
-    .catch(function (err) {
-      console.log(err);
-    }); 
+      .then((response) => response.json())
+      .then(function (response) {
+        console.log("ok request");
+        console.log(response.payload);
+        setUserInfo(response.payload);
+      })
+      .catch(function (err) {
+        console.log(err);
+      });
   };
 
   useEffect(() => {
-    console.log("in useEffect ok")
+    console.log("in useEffect ok");
     fetchUser();
     console.log(userInfo);
   }, []);
-    const confirmCadidacy = (info) => {
-      axios
-        .patch(`http://localhost:4000/api/users/register/${userId}`, info)
-        .then(function (response) {
-          console.log("Ok");
-          console.log(response);
-          setUserInfo(false);
-          setSendData(true);
-        })
-        .catch(function (err) {
-          console.log(err);
-        });
-    };
-
+  const confirmCadidacy = (info) => {
+    axios
+      .patch(`http://localhost:4000/api/users/register/${userId}`, info)
+      .then(function (response) {
+        console.log("Ok");
+        console.log(response);
+        setUserInfo(false);
+        setSendData(true);
+      })
+      .catch(function (err) {
+        console.log(err);
+      });
+  };
 
   return (
     <Container className={classes.paddingContainer}>
-      <h1>Iscrizione al corso "{nameCourse}"</h1>
-      <Paper elevation={3} className={classes.paddingForm}>
-        {userInfo ?
-        <FormSendCandidacy
-          data={userInfo}
-          onSubmit={(e) => confirmCadidacy(e)}
-        />
-        :
-        sendData ? <ConfirmSingIn /> :'loading'
-        }
-      </Paper>
+      <Grid container alignItems="center" justify="center">
+        <Grid item xs={12} sm={12} md={10} lg={8}>
+          <Paper elevation={3} className={classes.paddingForm}>
+            <h1 style={{ marginTop: "0px" }}>
+              Iscrizione al corso "{nameCourse}"
+            </h1>
+            {userInfo ? (
+              <FormSendCandidacy
+                data={userInfo}
+                onSubmit={(e) => confirmCadidacy(e)}
+              />
+            ) : sendData ? (
+              <ConfirmSingIn />
+            ) : (
+              "loading"
+            )}
+          </Paper>
+        </Grid>
+      </Grid>
     </Container>
   );
 }
